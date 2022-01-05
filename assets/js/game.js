0c56188f2a65a7229648ee632d6dd2285dff6ccd
rharris529 <jordandine@gmail.com>
2022-01-04 18:05:47 -0600

    Your Robot's Name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// Game Begins 
+var playerName = window.prompt("What is your robot's name?");
+// No quotation marks means the content in the prompt is stored in the console
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message")
+// this will do math and log 20
+console.log(10+10);
+// what is this 
+console.log("Our robot's name is" + playerName)
+
+// This creates a function named "fight" 
+function fight() {
+    window.alert("The fight has begun");
+
+}
+// fight();
